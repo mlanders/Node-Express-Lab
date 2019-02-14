085138e4e70322a555e6ddf811f2d4e236a37829
Mike Landers <landers.mike@gmail.com>
2019-02-14 12:28:52 -0800

    heroku setup

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const server = require('./server');
 
-server.listen(5000, () => {
-	console.log(`\n* Server running on httpp://localhost:5000 *\n`);
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+	console.log(`\n* Server running on httpp://localhost:${port} *\n`);
 });
